0f7b35369210fd71842af0ec713a5bc8b0ea91b8
Brainybrian316 <brainybrian316@gmail.com>
2022-07-13 17:14:39 -0400

    reinstalling packages

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
