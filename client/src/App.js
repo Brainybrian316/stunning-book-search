f1cb839d5a5fcbcc3299c3bd81f730cbacc7f192
Brainybrian316 <brainybrian316@gmail.com>
2022-07-13 20:26:45 -0400

    edited app.js and package.json

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,12 @@ const httpLink = createHttpLink({
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
+    headers: {
     ...headers,
     authorization: token ? `Bearer ${token}` : '',
   }
-})
+};
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
